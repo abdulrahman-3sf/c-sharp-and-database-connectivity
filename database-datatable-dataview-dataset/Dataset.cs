cb7a25f706f4db11273a51c0705169b8550db18c
Abdulrahman Alassaf. <163548732+abdulrahman-3sf@users.noreply.github.com>
2025-06-30 17:22:53 +0300

    Appling on DataSet

---
+++ database-datatable-dataview-dataset/Dataset.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace database_datatable_dataview_dataset
+{
+    internal class Dataset
+    {
+        static void Main(string[] args)
+        {
+            DataTable employeeDT = new DataTable();
+            employeeDT.Columns.Add("ID", typeof(int));
+            employeeDT.Columns.Add("Name", typeof(string));
+            employeeDT.Columns.Add("Country", typeof(string));
+            employeeDT.Columns.Add("Salary", typeof(Double));
+            employeeDT.Columns.Add("Date", typeof(DateTime));
+
+            employeeDT.Rows.Add(1, "Ayman", "Syria", 60000, DateTime.Now);
+            employeeDT.Rows.Add(2, "Ahmed", "Saudi Arabia", 22222, DateTime.Now);
+            employeeDT.Rows.Add(3, "Fasil", "Jordan", 13030, DateTime.Now);
+            employeeDT.Rows.Add(4, "Mohammed", "Egypt", 3300, DateTime.Now);
+            employeeDT.Rows.Add(5, "Khaled", "Saudi Arabia", 55500, DateTime.Now);
+
+            DataTable departmentDT = new DataTable();
+            departmentDT.Columns.Add("DepartmentID", typeof(int));
+            departmentDT.Columns.Add("DepratmentName", typeof(string));
+
+            departmentDT.Rows.Add(1, "IT");
+            departmentDT.Rows.Add(2, "HR");
+            departmentDT.Rows.Add(3, "Markiting");
+
+            foreach (DataRow recordRow in employeeDT.Rows)
+            {
+                Console.WriteLine($"ID: {recordRow["ID"]},\tName: {recordRow["Name"]},\t\tCountry: {recordRow["Country"]}");
+            }
+
+            foreach (DataRow recordRow in departmentDT.Rows)
+            {
+                Console.WriteLine($"ID: {recordRow["DepartmentID"]},\tName: {recordRow["DepratmentName"]}");
+            }
+
+
+            DataSet dataSet1 = new DataSet();
+
+            dataSet1.Tables.Add(employeeDT);
+            dataSet1.Tables.Add(departmentDT);
+
+            Console.WriteLine("\n\n------- Print Employees Data from DataSet -------");
+
+            foreach (DataRow recordRow in dataSet1.Tables[0].Rows)
+            {
+                Console.WriteLine($"ID: {recordRow["ID"]},\tName: {recordRow["Name"]},\t\tCountry: {recordRow["Country"]}");
+            }
+
+            Console.WriteLine("\n\n------- Print Departments Data from DataSet -------");
+
+            foreach (DataRow recordRow in dataSet1.Tables[1].Rows)
+            {
+                Console.WriteLine($"ID: {recordRow["DepartmentID"]},\tName: {recordRow["DepratmentName"]}");
+            }
+        }
+    }
+}
--- database-datatable-dataview-dataset/Dataview.cs
+++ database-datatable-dataview-dataset/Dataview.cs
@@ -6,56 +6,55 @@ namespace database_datatable_dataview_dataset
 {
     internal class Dataview
     {
+        //static void Main(string[] args)
+        //{
+        //    DataTable employeeDT = new DataTable();
+        //    employeeDT.Columns.Add("ID", typeof(int));
+        //    employeeDT.Columns.Add("Name", typeof(string));
+        //    employeeDT.Columns.Add("Country", typeof(string));
+        //    employeeDT.Columns.Add("Salary", typeof(Double));
+        //    employeeDT.Columns.Add("Date", typeof(DateTime));
 
-        static void Main(string[] args)
-        {
-            DataTable employeeDT = new DataTable();
-            employeeDT.Columns.Add("ID", typeof(int));
-            employeeDT.Columns.Add("Name", typeof(string));
-            employeeDT.Columns.Add("Country", typeof(string));
-            employeeDT.Columns.Add("Salary", typeof(Double));
-            employeeDT.Columns.Add("Date", typeof(DateTime));
+        //    employeeDT.Rows.Add(1, "Ayman", "Syria", 60000, DateTime.Now);
+        //    employeeDT.Rows.Add(2, "Ahmed", "Saudi Arabia", 22222, DateTime.Now);
+        //    employeeDT.Rows.Add(3, "Fasil", "Jordan", 13030, DateTime.Now);
+        //    employeeDT.Rows.Add(4, "Mohammed", "Egypt", 3300, DateTime.Now);
+        //    employeeDT.Rows.Add(5, "Khaled", "Saudi Arabia", 55500, DateTime.Now);
 
-            employeeDT.Rows.Add(1, "Ayman", "Syria", 60000, DateTime.Now);
-            employeeDT.Rows.Add(2, "Ahmed", "Saudi Arabia", 22222, DateTime.Now);
-            employeeDT.Rows.Add(3, "Fasil", "Jordan", 13030, DateTime.Now);
-            employeeDT.Rows.Add(4, "Mohammed", "Egypt", 3300, DateTime.Now);
-            employeeDT.Rows.Add(5, "Khaled", "Saudi Arabia", 55500, DateTime.Now);
-
-            foreach (DataRow recordRow in employeeDT.Rows)
-            {
-                Console.WriteLine($"ID: {recordRow["ID"]},\tName: {recordRow["Name"]},\t\tCountry: {recordRow["Country"]}");
-            }
-
-
-            // DataView
-            DataView employeeDV = employeeDT.DefaultView;
-
-            Console.WriteLine("\n\n------- Print Rows Using DataView -------");
-            for (int i = 0; i < employeeDV.Count; i++)
-            {
-                Console.WriteLine($"ID: {employeeDV[i][0]},\tName: {employeeDV[i][1]},\t\tCountry: {employeeDV[i][2]}");
-            }
+        //    foreach (DataRow recordRow in employeeDT.Rows)
+        //    {
+        //        Console.WriteLine($"ID: {recordRow["ID"]},\tName: {recordRow["Name"]},\t\tCountry: {recordRow["Country"]}");
+        //    }
 
 
-            // Filter on DataView
-            employeeDV.RowFilter = "Country = 'Saudi Arabia'";
+        //    // DataView
+        //    DataView employeeDV = employeeDT.DefaultView;
 
-            Console.WriteLine("\n\n------- Filter by Country on DataView -------");
-            for (int i = 0; i < employeeDV.Count; i++)
-            {
-                Console.WriteLine($"ID: {employeeDV[i][0]},\tName: {employeeDV[i][1]},\t\tCountry: {employeeDV[i][2]}");
-            }
+        //    Console.WriteLine("\n\n------- Print Rows Using DataView -------");
+        //    for (int i = 0; i < employeeDV.Count; i++)
+        //    {
+        //        Console.WriteLine($"ID: {employeeDV[i][0]},\tName: {employeeDV[i][1]},\t\tCountry: {employeeDV[i][2]}");
+        //    }
 
 
-            // Sort on DataView
-            employeeDV.Sort = "Name asc";
+        //    // Filter on DataView
+        //    employeeDV.RowFilter = "Country = 'Saudi Arabia'";
 
-            Console.WriteLine("\n\n------- Sort by Name asc on DataView -------");
-            for (int i = 0; i < employeeDV.Count; i++)
-            {
-                Console.WriteLine($"ID: {employeeDV[i][0]},\tName: {employeeDV[i][1]},\t\tCountry: {employeeDV[i][2]}");
-            }
-        }
+        //    Console.WriteLine("\n\n------- Filter by Country on DataView -------");
+        //    for (int i = 0; i < employeeDV.Count; i++)
+        //    {
+        //        Console.WriteLine($"ID: {employeeDV[i][0]},\tName: {employeeDV[i][1]},\t\tCountry: {employeeDV[i][2]}");
+        //    }
+
+
+        //    // Sort on DataView
+        //    employeeDV.Sort = "Name asc";
+
+        //    Console.WriteLine("\n\n------- Sort by Name asc on DataView -------");
+        //    for (int i = 0; i < employeeDV.Count; i++)
+        //    {
+        //        Console.WriteLine($"ID: {employeeDV[i][0]},\tName: {employeeDV[i][1]},\t\tCountry: {employeeDV[i][2]}");
+        //    }
+        //}
     }
 }
